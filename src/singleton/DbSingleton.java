df6596ec01978f845ed85f00d0227f64ef104910
Javier <framirez76@uabc.edu.mx>
2021-04-12 21:28:40 -0700

    Make the class lazy loading

--- src/singleton/DbSingleton.java
+++ src/singleton/DbSingleton.java
@@ -11,13 +11,16 @@ package singleton;
  */
 public class DbSingleton {
     
-    private static DbSingleton instance = new DbSingleton();
+    private static DbSingleton instance = null;
     
     private DbSingleton(){
         
     }
     
     public static DbSingleton getInstance(){
+        if(instance == null){
+            instance = new DbSingleton();
+        }
         return instance;
     }
 }
